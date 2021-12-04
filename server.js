330bc92aa73c9f66655b5374ba6180463cd9e8e8
acquahsamuel <iamsamuelacquah@gmail.com>
2021-12-04 23:52:19 +0000

    server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/workport-client'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/workport-client/index.html'));
 });
 
 
