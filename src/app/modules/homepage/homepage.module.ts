b28a1632e5699d2b06fdc5670cef24db3048ae90
acquahsamuel <iamsamuelacquah@gmail.com>
2021-12-06 16:55:18 +0000

    [WIP] +> homepage component

--- src/app/modules/homepage/homepage.module.ts
+++ src/app/modules/homepage/homepage.module.ts
@@ -6,13 +6,16 @@ import { AboutComponent } from './pages/about/about.component';
 import { HomepageComponent } from './homepage.component';
 import { SharedModule } from 'src/app/shared/modules/shared.module';
 import { HomepageRoutingModule } from './homepage-routing.module';
-
+import { NavigationComponent } from 'src/app/shared/components/navigation/navigation.component';
+import { HeaderComponent } from 'src/app/shared/components/header/header.component';
 
 @NgModule({
     declarations: [
         FaqComponent,
         AboutComponent,
         HomepageComponent,
+        NavigationComponent,
+        HeaderComponent
     ],
     imports: [
         CommonModule,
