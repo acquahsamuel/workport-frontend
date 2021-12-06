9d86d4e9178f89c02fb93cf2dea6999686697870
acquahsamuel <iamsamuelacquah@gmail.com>
2021-12-06 13:37:56 +0000

    add modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,34 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//
+import { AdminModule } from './modules/admin/admin.module';
+import { UserAuthModule } from './modules/user-auth/user-auth.module';
+import { HomepageModule } from './modules/homepage/homepage.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
+
+    //TODO NOT BEST APPROACH (REFACTOR)
+    AdminModule,
+    UserAuthModule,
+    DashboardModule,
+    HomepageModule,
+
     BrowserAnimationsModule,
   ],
 
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
