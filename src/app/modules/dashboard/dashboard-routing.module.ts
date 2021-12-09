b0126a8529d4c47d1415d87d146b1ed070b20e6f
acquahsamuel <iamsamuelacquah@gmail.com>
2021-12-09 14:54:20 +0000

    dashboard routing

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppRoutes } from 'src/app/shared/constants/app.constants';
+import { AuthGuardService } from 'src/app/shared/services/auth-guard.service';
+import { AddCompanyComponent } from '../admin/pages/add-company/add-company.component';
+import { AddJobComponent } from '../admin/pages/add-job/add-job.component';
+import { AddUserComponent } from '../admin/pages/add-user/add-user.component';
+import { AllStatisticsComponent } from '../admin/pages/all-statistics/all-statistics.component';
+import { AllUsersComponent } from '../admin/pages/all-users/all-users.component';
+import { ApproveJobPostingComponent } from '../admin/pages/approve-job-posting/approve-job-posting.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
@@ -7,6 +15,7 @@ const routes: Routes = [
     path: '',
     component: DashboardComponent,
     children: [
+      
       { path: '', component: DashboardComponent },
     ]
   },
