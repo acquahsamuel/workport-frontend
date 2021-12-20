0d22c042b2f991ad10d284a32ec93228044613c4
acquahsamuel <iamsamuelacquah@gmail.com>
2021-12-20 23:49:08 +0000

    dashboard component

--- src/app/modules/dashboard/pages/post-job/post-job.component.ts
+++ src/app/modules/dashboard/pages/post-job/post-job.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+
+ interface Food{
+  value : string;
+  viewValue : string;
+}
+
+interface Job{
+  
+}
+
 @Component({
   selector: 'app-post-job',
   templateUrl: './post-job.component.html',
   styleUrls: ['./post-job.component.scss']
 })
+
+
 export class PostJobComponent implements OnInit {
+foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Web Development'},
+    {value: 'pizza-1', viewValue: 'Design'},
+    {value: 'tacos-2', viewValue: 'Product'},
+    {value: 'steak-0', viewValue: 'Customer Service'},
+    {value: 'pizza-1', viewValue: 'Data'},
+    {value: 'tacos-2', viewValue: 'Sales'},
+    {value: 'steak-0', viewValue: 'DevOps/SysAdmin'},
+    {value: 'pizza-1', viewValue: 'Business'},
+    {value: 'tacos-2', viewValue: 'Finance'},
+    {value: 'steak-0', viewValue: 'Legal'},
+    {value: 'pizza-1', viewValue: 'Human Resources'},
+    {value: 'tacos-2', viewValue: 'Medical'},
+    {value: 'steak-0', viewValue: 'Teaching'},
+    {value: 'pizza-1', viewValue: 'Everything Else'},    
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
