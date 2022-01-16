6450cc1694950f4a5c3a0649d6f100536f032ada
acquahsamuel <iamsamuelacquah@gmail.com>
2022-01-16 22:51:43 +0000

    dashboard

--- src/app/modules/dashboard/pages/post-job/post-job.component.ts
+++ src/app/modules/dashboard/pages/post-job/post-job.component.ts
@@ -19,20 +19,20 @@ interface Job{
 
 export class PostJobComponent implements OnInit {
 foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Web Development'},
-    {value: 'pizza-1', viewValue: 'Design'},
-    {value: 'tacos-2', viewValue: 'Product'},
-    {value: 'steak-0', viewValue: 'Customer Service'},
-    {value: 'pizza-1', viewValue: 'Data'},
-    {value: 'tacos-2', viewValue: 'Sales'},
-    {value: 'steak-0', viewValue: 'DevOps/SysAdmin'},
-    {value: 'pizza-1', viewValue: 'Business'},
-    {value: 'tacos-2', viewValue: 'Finance'},
-    {value: 'steak-0', viewValue: 'Legal'},
-    {value: 'pizza-1', viewValue: 'Human Resources'},
-    {value: 'tacos-2', viewValue: 'Medical'},
-    {value: 'steak-0', viewValue: 'Teaching'},
-    {value: 'pizza-1', viewValue: 'Everything Else'},    
+    // {value: 'steak-0', viewValue: 'Web Development'},
+    // {value: 'pizza-1', viewValue: 'Design'},
+    // {value: 'tacos-2', viewValue: 'Product'},
+    // {value: 'steak-0', viewValue: 'Customer Service'},
+    // {value: 'pizza-1', viewValue: 'Data'},
+    // {value: 'tacos-2', viewValue: 'Sales'},
+    // {value: 'steak-0', viewValue: 'DevOps/SysAdmin'},
+    // {value: 'pizza-1', viewValue: 'Business'},
+    // {value: 'tacos-2', viewValue: 'Finance'},
+    // {value: 'steak-0', viewValue: 'Legal'},
+    // {value: 'pizza-1', viewValue: 'Human Resources'},
+    // {value: 'tacos-2', viewValue: 'Medical'},
+    // {value: 'steak-0', viewValue: 'Teaching'},
+    // {value: 'pizza-1', viewValue: 'Everything Else'},    
   ];
 
   constructor() { }
